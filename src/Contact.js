41c9b7662d066ba92d9e3eaafcaa735490775874
Nonny <onyejekwe.harddisk20@gmail.com>
2020-01-01 08:27:25 +0100

    temp

---
+++ src/Contact.js
@@ -0,0 +1,64 @@
+{/* <section class="contact" data-section="contact">
+				<div class="narrow-content">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 col-md-pull-3" >
+							<span class="heading-meta">Get in Touch</span>
+							<h2 class="heading">Contact</h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-5">
+							<div class="feature feature-sm" >
+								<div class="icon">
+									<i class="icon-globe-outline"></i>
+								</div>
+								<div class="text">
+									<p><a href="#">admin@jequex.com.ng</a></p>
+								</div>
+							</div>
+
+							<div class="feature feature-sm" >
+								<div class="icon">
+									<i class="icon-map"></i>
+								</div>
+								<div class="text">
+									<p>16 Allan Ekwerekwu street, inland town, 3-3, Onitsha</p>
+								</div>
+							</div>
+
+							<div class="feature feature-sm" >
+								<div class="icon">
+									<i class="icon-phone"></i>
+								</div>
+								<div class="text">
+									<p><a href="tel://">+234 814 167 6170</a></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-7 col-md-push-1">
+							<div class="row">
+								<div class="col-md-10 col-md-offset-1 col-md-pull-1" >
+									<form action="">
+										<div class="form-group">
+											<input type="text" class="form-control" placeholder="Name">
+										</div>
+										<div class="form-group">
+											<input type="text" class="form-control" placeholder="Email">
+										</div>
+										<div class="form-group">
+											<input type="text" class="form-control" placeholder="Subject">
+										</div>
+										<div class="form-group">
+											<textarea name="" id="message" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+										</div>
+										<div class="form-group">
+											<input type="submit" class="btn btn-primary btn-send-message" value="Send Message">
+										</div>
+									</form>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</section> */}
