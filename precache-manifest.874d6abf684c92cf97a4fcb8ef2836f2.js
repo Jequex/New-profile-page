e3a6f7cbbb8991f2a2956a43ae7b0584137cbe3a
Nonny <onyejekwe.harddisk20@gmail.com>
2020-02-03 12:38:42 +0100

    Updates

---
+++ precache-manifest.874d6abf684c92cf97a4fcb8ef2836f2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d42cf2abb18afe8bf5ec2612c36b05",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "c1b23d68519be8a52b2c",
+    "url": "/react-gh-pages/static/css/main.b7034444.chunk.css"
+  },
+  {
+    "revision": "9ed995b2a6fac8405dc0",
+    "url": "/react-gh-pages/static/js/2.be4013d6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-gh-pages/static/js/2.be4013d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1b23d68519be8a52b2c",
+    "url": "/react-gh-pages/static/js/main.6bfbdca5.chunk.js"
+  },
+  {
+    "revision": "f581914669d6fcff7e3d",
+    "url": "/react-gh-pages/static/js/runtime-main.f93e5430.js"
+  },
+  {
+    "revision": "179f3cb98fcced59803adb6edfc3c9a5",
+    "url": "/react-gh-pages/static/media/Image2.179f3cb9.png"
+  },
+  {
+    "revision": "03e91f122aa5fd425abbe23c85546eb0",
+    "url": "/react-gh-pages/static/media/Linearicons-Free.03e91f12.woff2"
+  },
+  {
+    "revision": "2781bf543c08d394dc78dec86989e3fd",
+    "url": "/react-gh-pages/static/media/Linearicons-Free.2781bf54.svg"
+  },
+  {
+    "revision": "2f3e9f80fff7d699dd3de6904d7d1647",
+    "url": "/react-gh-pages/static/media/Linearicons-Free.2f3e9f80.ttf"
+  },
+  {
+    "revision": "65060723fe964f85afa0a82d0bb78cf9",
+    "url": "/react-gh-pages/static/media/Linearicons-Free.65060723.woff"
+  },
+  {
+    "revision": "b9b7f23cb61b1f503e1249b63d980448",
+    "url": "/react-gh-pages/static/media/Linearicons-Free.b9b7f23c.eot"
+  },
+  {
+    "revision": "a037f4e0b7cb8e21042b5b78305192c8",
+    "url": "/react-gh-pages/static/media/about.a037f4e0.png"
+  },
+  {
+    "revision": "f89a068609ef9f1400d8742d1c9faf8e",
+    "url": "/react-gh-pages/static/media/big.f89a0686.jpg"
+  },
+  {
+    "revision": "fe37dda36632bbd1d8a451f55f826abe",
+    "url": "/react-gh-pages/static/media/big2.fe37dda3.jpg"
+  },
+  {
+    "revision": "06c8e05aeb458266ade1085896c0332f",
+    "url": "/react-gh-pages/static/media/icomoon.06c8e05a.svg"
+  },
+  {
+    "revision": "8275504f5b9e186b54b8d84ebdedc2b4",
+    "url": "/react-gh-pages/static/media/icomoon.8275504f.eot"
+  },
+  {
+    "revision": "dd6d51dddf740923fe9c94859aeee2bf",
+    "url": "/react-gh-pages/static/media/icomoon.dd6d51dd.woff"
+  },
+  {
+    "revision": "ee8d016f1bfe9220bee450ff02b59624",
+    "url": "/react-gh-pages/static/media/icomoon.ee8d016f.ttf"
+  }
+]);
